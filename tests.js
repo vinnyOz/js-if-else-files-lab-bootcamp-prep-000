7bbf8a8b62c584e7408b35526fa80833e20a42a4
vincent ndisi <vndis1298@yahoo.com>
2018-08-17 23:40:15 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -2,9 +2,12 @@ var index = require("./index.js")
 
 if (index.js === "joe") {
     console.log("The name is joe")
-
+if (name ===                                         "joe") {
+  console.log("the name is correct")
+}
 
 if (index.height === 70) {
     console.log("The height is 70")
+    
 
 
